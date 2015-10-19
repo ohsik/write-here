1f0ca88de8a3a12cc838acecf4403e32833ad9ca
O <o@ohsikpark.com>
2015-10-19 18:01:04 -0500

    Working on editing a post
    work in progress

--- write-here.php
+++ write-here.php
@@ -196,7 +196,7 @@ function write_here_dashboard(){
         ?>
             <li>
                 <p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-                <p class="wh-post-meta"><i><?php echo get_the_date(); ?></i> <span class="wh-edit">Edit / Delete</span></p>
+                <p class="wh-post-meta"><i><?php echo get_the_date(); ?></i> <span class="wh-edit"><a href="http://localhost:8888/WordPress/edit-post/?post_id=<?php echo get_the_ID(); ?>">Edit</a> / <a href="">Delete</a></span></p>
             </li>
         <?php           
         endwhile;
@@ -235,8 +235,129 @@ function write_here_dashboard(){
     http://wordpress.stackexchange.com/questions/17400/how-can-i-edit-a-post-from-the-frontend
     ------------------------------------------------------------------
 */
+function write_here_edit_form(){
+    
+    $post_id = $_GET['post_id'];
+
+    if ($post_id){
+    
+        $post_to_edit = get_post($post_id);
+    
+        // Get existing tags for the post
+        function get_existing_tags($post_id){
+            $tags = wp_get_post_tags($post_id); 
+            $iflast = $tags;
+            foreach ($tags as $tag){ 
+                echo $tag->name; 
+                if(next($iflast)){
+                    echo ', ';
+                }
+            } 
+        }
+
+?>
+<div class="write-here">
+    <?php write_here_show_error_messages(); ?>
+    <form id="new_post" name="new_post" method="post" action="">
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" value="<?php echo $post_to_edit->post_title; ?>" />
+
+        <label for="wh_content">Content</label>
+        <?php
+            $content = $post_to_edit->post_content;
+            $editor_id = 'wh_content';
+            wp_editor( $content, $editor_id );
+        ?>
+
+        <label for="cat">Category</label>
+        <?php 
+            $cat = wp_get_post_terms( $post_to_edit->ID, 'category');
+            if($cat){
+                wp_dropdown_categories( 'show_option_none=Category&taxonomy=category&selected='.$cat[0]->term_id); 
+            }else{
+                wp_dropdown_categories( 'show_option_none=Category&taxonomy=category&hide_empty=0' );
+            } 
+        ?>
+
+        <label for="post_tags">Tags</label>
+        <input type="text" id="post_tags" name="post_tags" value="<?php get_existing_tags($post_id); ?>" />
+        
+        <label for="date">Date</label>
+        <div id="timestampdiv" class="hide-if-js"><?php write_here_time(0, 0, 5); ?></div>
+
+        <input type="submit" value="Publish" id="submit" name="submit" />
+        <input type="hidden" name="action" value="write_here_edit_post" />
+        <input type="hidden" name="pid" value="<?php echo $post_to_edit->ID; ?>" />
+        <?php wp_nonce_field( 'new-post' ); ?>
+    </form>
+</div>
+<?php  
+    }else{ echo"Nonono"; }
+}
+
+/*
+**  Process data from edit form
+    http://codex.wordpress.org/Function_Reference/wp_insert_post
+*/
+function write_here_edit_post() {
+    if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) &&  $_POST['action'] == "write_here_edit_post") {
+        // Set default date on the post
+        $postdate = date('Y-m-d H:i:s');
+        // Get values from front end form
+        $wh_month   = $_POST['mm'];
+        $wh_day     = $_POST['jj'];
+        $wh_year    = $_POST['aa'];
+        $wh_hour    = $_POST['hh'];
+        $wh_min     = $_POST['mn'];
+        $wh_sec     = $_POST['ss'];
+        $title      =  wp_strip_all_tags($_POST['title']);
+        $content    = $_POST['wh_content'];
+        $tags       = $_POST['post_tags'];
+        $cat        = $_POST['cat'];
+        
+        // Server side validation
+        if ($title == '') {
+            write_here_errors()->add('title_not_vaild', __('Title not valid'));
+        }
+        if ($content == '') {
+            write_here_errors()->add('content_not_vaild', __('Content not valid'));
+        }
+        if ( strlen($wh_month) != 2 || strlen($wh_day) != 2 || strlen($wh_year) != 4 || strlen($wh_hour) != 2 || strlen($wh_min) != 2 || !ctype_digit($wh_month) || !ctype_digit($wh_day) || !ctype_digit($wh_year) || !ctype_digit($wh_hour) || !ctype_digit($wh_min) ) {
+            write_here_errors()->add('date_not_vaild', __('Date not valid'));
+        } else {
+            $postdate = $wh_year.'-'.$wh_month.'-'.$wh_day.' '.$wh_hour.':'.$wh_min.':'.$wh_sec;
+        }
 
 
+        // Add the content of the form to $post as an array
+        $new_post = array(
+            'post_title'    => $title,
+            'post_content'  => $content,
+            'post_category'	=> array($cat),     // Default empty.
+            'tags_input'    => array($tags),    // Default empty.
+            'post_status'   => 'publish',       // Choose: publish, preview, future, draft, etc. Default 'draft'.
+            'post_date'     => $postdate,       // The time post was made.
+            'post_date_gmt' => $postdate        // The time post was made, in GMT.
+        );
+
+        $errors = write_here_errors()->get_error_messages();
+        
+        // only create post if there are no errors
+		if(empty($errors)) {
+            //save the new post and return its ID
+            $post_id = wp_update_post($new_post);
+            
+            if($post_id) {
+                // This will redirect you to the newly created post (Using GUID)
+                $post = get_post($post_id);
+                wp_redirect($post->guid);
+                exit();
+            }
+        }
+        
+    }
+}
+add_action('init', 'write_here_edit_post');
 
 
 
@@ -280,3 +401,23 @@ function dashboard_write_here(){
     }
 }
 add_shortcode('write-here-dashboard', 'dashboard_write_here');
+
+/*
+**  Add a shortcode for edit formß
+    [write-here-edit]
+    https://codex.wordpress.org/Function_Reference/add_shortcode
+*/
+function edit_write_here(){
+    // Load CSS & JS files
+    global $write_here_load_assets;
+    $write_here_load_assets = true;
+    
+    // Show only to logged in users
+    if ( is_user_logged_in() ) {
+        $output = write_here_edit_form();
+        return $output;
+    }else{
+        echo 'Please Sign in to continue...';
+    }
+}
+add_shortcode('write-here-edit', 'edit_write_here');
